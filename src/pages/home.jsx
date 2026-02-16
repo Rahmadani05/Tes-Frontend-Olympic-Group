3bf2bf814a7e3ae4343f382c5f855c29b82325d5
Rahmadani Suryanto Dwi Putra <rahmadanisuryanto05@gmail.com>
2026-02-16 14:13:14 +0700

    Fix Debugging v2

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { searchMovies } from "../api";
+import { searchMovies } from "../API";
 import MovieCard from "../components/moviecard";
 
 const Home = () => {
